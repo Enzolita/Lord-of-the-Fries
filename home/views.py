5b7788a67cfbfeb61e3d691c3631294629282d2e
Elena Hanna <hanna_elena@live.se>
2024-07-23 10:53:14 +0000

    Add python files

---
+++ home/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+
+def home_page(request):
+    return HttpResponse("Hello, Home!")
+    template_name = "home/index.html"
+    paginate_by = 6
