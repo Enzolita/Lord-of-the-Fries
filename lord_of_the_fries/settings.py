7bf30be831403e746d65e4436b6e38476bc1b9f4
Elena Hanna <hanna_elena@live.se>
2024-08-01 10:46:28 +0000

    Add heroku to allowed host

--- lord_of_the_fries/settings.py
+++ lord_of_the_fries/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-enzolita-lordofthefries-m97etwxax3l.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['.herokuapp.com', '8000-enzolita-lordofthefries-m97etwxax3l.ws.codeinstitute-ide.net']
 
 
 # Application definition
