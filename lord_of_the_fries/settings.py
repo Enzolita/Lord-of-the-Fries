bae7f37c359e4426ed7445f1330fa34d3d0eb97a
Elena Hanna <hanna_elena@live.se>
2024-08-01 10:38:42 +0000

    Add secret key to .env

--- lord_of_the_fries/settings.py
+++ lord_of_the_fries/settings.py
@@ -25,7 +25,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rgs@_92a*%$r!+08t++6as@!2p$)g)g^)5$)id5&rmape*-(&c'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
