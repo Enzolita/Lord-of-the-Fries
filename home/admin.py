10accacc3ce2602b302b8af7aaf6f53532d42312
Elena Hanna <hanna_elena@live.se>
2024-08-14 13:20:46 +0000

    Add model to admin

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Recipe, Post, Ingredient, Author
 
 # Register your models here.
+admin.site.register(Comment)
+admin.site.register(Recipe)
+admin.site.register(Author)
+admin.site.register(Ingredient)
