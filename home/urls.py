d7f3f16d06f42a716202aa7e57f4bd9d021d58f3
Elena Hanna <hanna_elena@live.se>
2024-08-19 11:59:40 +0000

    Fix urls

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,11 @@
 from . import views
+from django.urls import path
+from .views import Index
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home_page),
-    path('about/',),
+    path('', Index.as_view(), name='home')
 ]   
